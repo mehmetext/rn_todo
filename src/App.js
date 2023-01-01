660ef5a9d68ef6711819143b7da2ab3e60c5a963
Mehmet Konukcu <mehmetext68@gmail.com>
2023-01-01 17:03:58 +0300

    src oluşturuldu

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import {Text, SafeAreaView} from 'react-native';
+import React from 'react';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+}
